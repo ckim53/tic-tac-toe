6a286170c4920929e86e8fcbae5ee9c6de3d5bb3
Christina <184446177+ckim53@users.noreply.github.com>
2025-02-19 14:34:38 -0800

    Restructure factory logic and ensure it works in console.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function createGameBoard() {
+function createGameBoard(player1, player2) {
     let tie = 0;
     let entries = 0;
     const rows = 3;
@@ -9,91 +9,82 @@ function createGameBoard() {
     const tieStatus = () => tie;
     const gameOver = () => tie++;
 
-    let array = Array.from({ length: rows }, () => Array(cols).fill(null));
+    const array = Array.from({ length: rows }, () => Array(cols).fill(null));
 
     const reset = function () {
         array = Array.from({ length: rows }, () => Array(cols).fill(null));
     }
-    return {array, tieStatus, gameOver, reset, addEntry, getEntries};
-};
 
-const createPlayer = function (playerNum, gameBoard) {
-    let marker = '';
-    let winner = 0;
-    playerNum == 1 ? marker = 'x' : marker = 'o';
+    player1.setTurn();
+    
+    const grid = document.getElementById('grid');
+    const children = grid.querySelectorAll('div');
+    let player = player1;
 
-    const getWinner = () => winner;
-
-    const playTurn = () => {
-        //assume in range for now
-        let userInput = prompt(`Player ${playerNum}: Pick a spot (0-8).`);
-        while (true) {
-             
-            if (userInput == null) {
-                gameBoard.endGame = 1;
-                break;
-            }
+    children.forEach((child, index) => {
+        child.addEventListener('click', function() {
+            player = (player1.getTurn() == 1) ? player1 : player2;
+            const marker = player.getMarker();
+            console.log(`player ${player.num} turn`);
             let row = 0;
             switch (true) {
-                case (userInput >= 0 && userInput <= 2):
+                case (index >= 0 && index <= 2):
                     row = 0;
                     break;
-                case (userInput >= 3 && userInput <= 5):
+                case (index >= 3 && index <= 5):
                     row = 1;
                     break;
-                case (userInput >= 6 && userInput <= 8):
+                case (index >= 6 && index <= 8):
                     row = 2;
                     break;
             }
-            if (gameBoard.array[row][userInput % 3] == null) {
-                gameBoard.array[row][userInput % 3] = marker;
-                gameBoard.addEntry();
-                break;
+            if (array[row][index % 3] == null) {
+                console.log(array);
+                array[row][index % 3] = marker;
+                addEntry();
+                console.log("valid spot.");
+                child.textContent = marker;
+                checkBoard();
+                player1.setTurn();
+                player2.setTurn();
             }
             else {
-                userInput = prompt("Invalid. Pick a spot (0-8).");
-                continue;
+                console.log("invalid spot. try again.");
             }
-        }
-        if (gameBoard.getEntries() == 9) {
-            gameBoard.gameOver();
-            console.log("It's a tie!");
-        }
-        checkRows();
-        checkColumns();
-        checkDiagonals();
-        if (winner == 1) {console.log(`Player ${playerNum} wins!`);}
-    }
+        });
+    });
 
     const checkRows = () => 
     { 
-        for (let i=0; i < gameBoard.array.length; i++) {
+        for (let i=0; i < array.length; i++) {
             let j = 0;
-            let marker = gameBoard.array[i][j];
-            if (marker == null) {
+            let current = array[i][j];
+            if (current == null) {
                 continue;
             }
-            while (gameBoard.array[i][j] == marker) {
+            while (array[i][j] == current) {
                 j++;
             }
             if (j==3) {
-                winner = 1;
+                player.setWinner();
+                console.log(`Player ${player.num} wins!`);
             }
         }
     }
 
     const checkColumns = () => 
     { 
-        for (let j=0; j < gameBoard.array.length; j++) {
+        for (let j=0; j < array.length; j++) {
             let i = 0;
-            let marker = gameBoard.array[i][j];
-            if (marker == null) {
+            let current = array[i][j];
+            if (current == null) {
                 continue;
             }
-            while (gameBoard.array[i][j] == marker) {
+            while (array[i][j] == current) {
                 i++;
                 if (i==3) {
-                    winner = 1;
+                    player.setWinner();
+                    console.log(`Player ${player.num} wins!`);
                     return;
                 }
             }
@@ -102,27 +93,44 @@ const createPlayer = function (playerNum, gameBoard) {
 
     const checkDiagonals = () => 
     { 
-        if ((gameBoard.array[1][1] != null) && ((gameBoard.array[0][0] == gameBoard.array[1][1] && 
-            gameBoard.array[1][1] == gameBoard.array[2][2]) ||
-            (gameBoard.array[0][2] == gameBoard.array[1][1] && 
-            gameBoard.array[1][1] == gameBoard.array[2][0]))) { 
-            winner = 1;
+        if ((array[1][1] != null) && ((array[0][0] == array[1][1] && 
+            array[1][1] == array[2][2]) ||
+            (array[0][2] == array[1][1] && 
+            array[1][1] == array[2][0]))) { 
+            player.setWinner();
+            console.log(`Player ${player.num} wins!`);
         }
     };
+    
+    const checkBoard = () => {
+        checkRows();
+        checkColumns();
+        checkDiagonals();
+    }
 
-    return {playTurn, getWinner};
+    return {array, tieStatus, gameOver, reset, addEntry, getEntries, checkBoard};
+}
+
+const createPlayer = function (num) {
+    let winner = 0;
+    let turn = 0;
+    let marker = (num == 1) ? 'X' : 'O';
+    
+    const getMarker = () => marker;
+    const getWinner = () => winner;
+    const setWinner = () => winner = 1;
+    const getTurn = () => turn;
+    const setTurn = () => turn = 1 - turn;
+
+    return {getMarker, num, getWinner, setWinner, setTurn, getTurn};
 }
 
 const playGame = function (gameBoard, player1, player2) {
-    let firstPlayer = true;
-    
-    while (gameBoard.tieStatus() == 0 && player1.getWinner() == 0 && player2.getWinner() == 0) {
-        firstPlayer ? player1.playTurn() : player2.playTurn();
-        firstPlayer = !firstPlayer;
-    }
-};
+    while ((gameBoard.tieStatus() == 0) && (player1.getWinner == 0) && (player2.getWinner == 0));
+    console.log("game ended.");
+}
 
-const myGameBoard = createGameBoard();
-const player1 = createPlayer(1, myGameBoard);
-const player2 = createPlayer(2, myGameBoard);
+const player1 = createPlayer(1);
+const player2 = createPlayer(2);
+const myGameBoard = createGameBoard(player1, player2);
 playGame(myGameBoard, player1, player2);
